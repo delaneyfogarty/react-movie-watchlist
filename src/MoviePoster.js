a1a2f04ae45834f187f5c2fbc9d602f336076e9e
Delaney Fogarty <delaney.fk@gmail.com>
2022-05-05 11:46:47 -0700

    movie poster component complete

--- src/MoviePoster.js
+++ src/MoviePoster.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-export default function MoviePoster() {
-  return <div>MoviePoster</div>;
+export default function MoviePoster({ title, director, year, posterColor, deleteMovie }) {
+  return (
+    <div
+      onClick={() => deleteMovie(title)}
+      className="movie-poster"
+      style={{ backgroundColor: posterColor }}
+    >
+      <h3>{title}</h3>
+      <p>{director}</p>
+      <p>{year}</p>
+    </div>
+  );
 }
