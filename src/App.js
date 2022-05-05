34042977062d5c44197edcdcde1e8bb0c994350a
Delaney Fogarty <delaney.fk@gmail.com>
2022-05-05 09:37:21 -0700

    skeleton of app.js complete and files set up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import MovieForm from './MovieForm';
+import MovieList from './MovieList';
+import MoviePoster from './MoviePoster';
 
 function App() {
+  const [allMovies, setAllMovies] = useState([]);
+  const [filteredMovies, setAllFilteredMovies] = useState([]);
+  const [movieFormTitle, setMovieFormTitle] = useState('');
+  const [movieFormDirector, setMovieFormDirector] = useState('');
+  const [movieFormYear, setMovieFormYear] = useState('');
+  const [movieFormColor, setMovieFormColor] = useState('');
+
+  function handleSubmitMovie(e) {
+    e.preventDefault;
+    setAllFilteredMovies([...allMovies]);
+  }
+
+  function handleDeleteMovie() {}
+
+  function handleFilteredMovies() {}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MovieForm
+          movieFormTitle={movieFormTitle}
+          setMovieFormTitle-={setMovieFormTitle}
+          movieFormDirector={movieFormDirector}
+          setMovieFormDirector={setMovieFormDirector}
+          movieFormYear={movieFormYear}
+          setMovieFormYear={setMovieFormYear}
+        />
+        <MovieList />
+        <MoviePoster movieFormColor={movieFormColor} setMovieFormColor={setMovieFormColor} />
       </header>
     </div>
   );
